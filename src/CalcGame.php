f9fca36ae9715a1afec43595b5243e532bafcbe8
mihaska1 <mihaska1@gmail.com>
2021-05-06 17:44:01 +0100

    New files for game brain-calc added

---
+++ src/CalcGame.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Brain\Games\CalcGame;
+
+use function Brain\Games\Engine\GetPlayerAnswer;
+
+const CALC_GAME_RULES = "What is the result of the expression?";
+
+function calcGame()
+{
+    $rand1 = rand(2, 100);
+    $rand2 = rand(2, 100);
+    $operator = array('*', '+', '-');
+    $randomOperator = $operator[rand(0, 2)];
+    switch ($randomOperator) {
+        case "+":
+            $correctAnswer = $rand1 + $rand2;
+            $question = "{$rand1} + {$rand2}";
+            return GetPlayerAnswer($question, $correctAnswer);
+        case "-":
+            $correctAnswer = $rand1 - $rand2;
+            $question = "{$rand1} - {$rand2}";
+            return GetPlayerAnswer($question, $correctAnswer);
+        case "*":
+            $correctAnswer = $rand1 * $rand2;
+            $question = "{$rand1} * {$rand2}";
+            return GetPlayerAnswer($question, $correctAnswer);
+    }
+    return 0;
+}
